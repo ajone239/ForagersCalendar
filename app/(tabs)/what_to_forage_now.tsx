7c9500e069477c77a49f40051bac4ffc6ddc398b
Austin Jones <ajones53.aj@gmail.com>
2024-12-23 14:22:31 -0600

    Add in a basic data fetching service

--- app/(tabs)/what_to_forage_now.tsx
+++ app/(tabs)/what_to_forage_now.tsx
@@ -1,12 +1,28 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 
 import { Collapsible } from '@/components/Collapsible';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
+import { Forageable, FetchForageablesService } from '@/services/FetchForageablesService';
+import { useEffect, useState } from 'react';
 
 export default function TabTwoScreen() {
+    const [forageables, setForageables] = useState<Forageable[] | null>(null);
+
+    const fetchForageableService = new FetchForageablesService();
+
+    const fetchData = async () => {
+        var data = await fetchForageableService.loadForageables();
+        setForageables(data)
+    }
+
+    useEffect(
+        () => { fetchData() },
+        []
+    );
+
     return (
         <ParallaxScrollView
             headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
@@ -23,20 +39,30 @@ export default function TabTwoScreen() {
                 <ThemedText type="title">What to forage now!</ThemedText>
             </ThemedView>
 
-            <ThemedText>
-                This screen will have a item view of current seasonal forageables.
-            </ThemedText>
-
             <Collapsible title="Mushrooms">
                 <ThemedText>
                     This screen will have a item view of current seasonal mushrooms.
                 </ThemedText>
             </Collapsible>
-            <Collapsible title="Fruit">
-                <ThemedText>
-                    This view will have a item view of current seasonal fruit.
-                </ThemedText>
-            </Collapsible>
+
+            {
+                forageables !== null &&
+                    forageables.length > 0 ? (
+                    <FlatList
+                        data={forageables}
+                        renderItem={({ item }) =>
+                            <ThemedText>
+                                {item.name}
+                            </ThemedText>
+                        }
+                    />
+                ) : (
+                    <ThemedText>
+                        There's no data yet!
+                    </ThemedText>
+                )
+            }
+
         </ParallaxScrollView>
     );
 }
---
+++ services/FetchForageablesService.ts
@@ -0,0 +1,112 @@
+export class FetchForageablesService {
+
+    async sleep(ms: number): Promise<void> {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    async loadForageables(): Promise<Forageable[]> {
+        await this.sleep(5000);
+
+        return forageables;
+    }
+
+}
+export enum ForagableClass {
+    Mushroom,
+    Fruit,
+    Leaf,
+    Stalk,
+}
+
+export enum Season {
+    Summer,
+    Fall,
+    Winter,
+    Spring,
+}
+
+export enum Region {
+    SouthEast,
+}
+
+export interface Forageable {
+    name: string;
+    class: ForagableClass;
+    season: Season;
+    region: Region;
+    location: string;
+}
+
+const forageables: Forageable[] = [
+    {
+        name: "Chanterelle",
+        class: ForagableClass.Mushroom,
+        season: Season.Summer,
+        region: Region.SouthEast,
+        location: "Deciduous forests near oak trees",
+    },
+    {
+        name: "Morel",
+        class: ForagableClass.Mushroom,
+        season: Season.Spring,
+        region: Region.SouthEast,
+        location: "Moist wooded areas and near dead trees",
+    },
+    {
+        name: "Blackberry",
+        class: ForagableClass.Fruit,
+        season: Season.Summer,
+        region: Region.SouthEast,
+        location: "Sunny edges of forests and trails",
+    },
+    {
+        name: "Persimmon",
+        class: ForagableClass.Fruit,
+        season: Season.Fall,
+        region: Region.SouthEast,
+        location: "Open fields or near wooded areas",
+    },
+    {
+        name: "Pine Needles",
+        class: ForagableClass.Leaf,
+        season: Season.Winter,
+        region: Region.SouthEast,
+        location: "Pine forests and coniferous trees",
+    },
+    {
+        name: "Wild Lettuce",
+        class: ForagableClass.Leaf,
+        season: Season.Spring,
+        region: Region.SouthEast,
+        location: "Shady and moist meadows",
+    },
+    {
+        name: "Cattail",
+        class: ForagableClass.Stalk,
+        season: Season.Summer,
+        region: Region.SouthEast,
+        location: "Edges of ponds and wetlands",
+    },
+    {
+        name: "Goldenrod",
+        class: ForagableClass.Stalk,
+        season: Season.Fall,
+        region: Region.SouthEast,
+        location: "Fields and open grassy areas",
+    },
+    {
+        name: "Elderberry",
+        class: ForagableClass.Fruit,
+        season: Season.Summer,
+        region: Region.SouthEast,
+        location: "Lowland thickets and stream banks",
+    },
+    {
+        name: "Reishi Mushroom",
+        class: ForagableClass.Mushroom,
+        season: Season.Fall,
+        region: Region.SouthEast,
+        location: "Base of hardwood trees, especially oaks",
+    },
+];
+
